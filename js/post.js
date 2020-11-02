7cc89937e7ab9d95f701306211e9e90cadc29f75
Chip Miller <wgmilleriii@gmail.com>
2020-11-02 08:35:45 -0500

    1
    2

--- js/post.js
+++ js/post.js
@@ -1,17 +1,23 @@
+var urlPrefix="http://localhost:8888/mandalaz/php/";
 $(document).on('click','#btnCreate', function() {
 
     var username=$("#username").val();
     var password=$("#password").val();
 
-    var url="php/post.php?username=" + username + "&password=" + password;
-    console.log("Posting to " + url);
+    url=cs("post.php?action=login&username=" + username + "&password=" + password);
    $.post(url)
       .done(function(data) {
         console.log(data);
         // $("#divEditPerson").html(data);
         // $("#divEditPerson")[0].scrollIntoView();        
-        console.log("Created!");
+        // console.log("Created!");
       });
 
   
 });
+
+function cs(s) {
+  console.log("-= Posting: =- ");  
+  console.log(urlPrefix + s);
+  return urlPrefix + s;
+}
--- php/post.php
+++ php/post.php
@@ -1,27 +1,89 @@
 <?php
 
-		$link = mysqli_init();
-		
-		$host="localhost";
-		$user="root";
-		$password="";
-		$db="mandalaz";
-		$port=3306;
+// Step 1. Connect to the database.
+
+$link = mysqli_init();
+
+$host="localhost";
+$user="root";
+$password="root";
+$db="mandalaz";
+$port=3306;
+
+@ $exists = mysqli_real_connect(
+   $link, 
+   $host, 
+   $user, 
+   $password, 
+   $db,
+   $port
+) ;
+
+if (!$exists) {
+	echo "Error connecting.";
+	die();
+} 
+
+
+// Step 2. Determine the action
+$action="";
+if (isset($_GET["action"])) {
+	$action=$_GET["action"];	
+} 
+
+switch ($action) {
+	case "login":
+		login();
+		break;
+	case "createUser":
+		createUser();
+		break;
+}
+
+// Step 3. Do the stuff.
+
+function dosql($sql) {
+	global $link;
+	$result=mysqli_query($link,$sql);
+	
+	return $result;
+
+}
+
+function login() {
+	$username=safe($_GET["username"]);
+	$password=safe($_GET["password"]);
+
+	$sql="select * from users where username='%s' and password='%s'";
+	$sql=sprintf($sql, $username, $password);
+	$result=dosql($sql);
+	$userid=0;
+	while($row = mysqli_fetch_array($result)) { 
+		$userid=$row["userid"];
+		$fullname=$row["fullname"];
+	}
+	
+	if ($userid>0) {
+		?>
+<script>
+goodLogin(<? echo $userid ?>, "<? echo $fullname ?>");
+</script>
+		<?
+	} else {
+		?>
+<script>
+badLogin();
+</script>
+		<?
+	}
+
+}
 
 
 
-
-		@ $exists = mysqli_real_connect(
-		   $link, 
-		   $host, 
-		   $user, 
-		   $password, 
-		   $db,
-		   $port
-		) ;
-
-		if (!$exists) {
-			echo "DB doesn't exist<BR>" + mysqli_error($link);
-		} else {
-			echo "Connected";
-		}
+// useful functions
+function safe($s) {
+	
+	$s=str_replace("'","",$s);
+	return $s;
+}
